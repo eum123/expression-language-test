9b5fd325005a9c48d76f8d32de45fa20cf6058ef
eummanjin <manjineum@hanwha.com>
2023-11-06 15:18:28 +0900

    add

--- src/test/java/janino/CompilerTest.java
+++ src/test/java/janino/CompilerTest.java
@@ -10,7 +10,9 @@ import org.codehaus.commons.compiler.util.resource.MapResourceFinder;
 import org.codehaus.commons.compiler.util.resource.Resource;
 import org.codehaus.commons.compiler.util.resource.StringResource;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.StopWatch;
 
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,6 +22,8 @@ public class CompilerTest {
         ICompiler compiler = CompilerFactoryFactory
                         .getDefaultCompilerFactory(CompilerTest.class.getClassLoader())
                         .newCompiler();
+
+
 
         // Store generated .class files in a Map:
         Map<String, byte[]> classes = new HashMap<String, byte[]>();
@@ -43,7 +47,80 @@ public class CompilerTest {
                 ClassLoader.getSystemClassLoader() // parent
         );
 
+        long start = System.currentTimeMillis();
+
         System.out.println(cl.loadClass("pkg1.A").getDeclaredMethod("meth").invoke(null));
+
+        System.out.println(System.currentTimeMillis() - start);
+
+        //Assert.assertEquals(77, cl.loadClass("pkg1.A").getDeclaredMethod("meth").invoke(null));
+    }
+
+
+    @Test
+    public void test1() throws Exception {
+
+        StopWatch stopWatch = new StopWatch("test1");
+        stopWatch.start("create");
+
+        ICompiler compiler = CompilerFactoryFactory
+                .getDefaultCompilerFactory(CompilerTest.class.getClassLoader())
+                .newCompiler();
+
+        stopWatch.stop();
+        stopWatch.start("compile");
+
+        // Store generated .class files in a Map:
+        Map<String, byte[]> classes = new HashMap<String, byte[]>();
+        compiler.setClassFileCreator(new MapResourceCreator(classes));
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("package pkg2; ");
+        builder.append("import java.util.HashMap; ");
+        builder.append("import java.util.Map; ");
+        builder.append("import java.lang.Integer; ");
+        builder.append("public class RuleId {");
+        builder.append("    public static Map<String, Object> meth(Map<String, Object> parameter) {");
+        builder.append("        Map<String, Object> map = new HashMap();");
+        builder.append("        if((Integer)parameter.get(\"홍길동\") > 0) {");
+        builder.append("            map.put(\"홍길동\", \"1\");");
+        builder.append("        }");
+        builder.append("        return map;");
+        builder.append("    }");
+        builder.append("}");
+
+        // Now compile two units from strings:
+        compiler.compile(new Resource[] {
+
+                new StringResource(
+                        "pkg2/RuleId.java",
+                        builder.toString()
+                )
+        });
+
+        // Set up a class loader that uses the generated classes.
+        ClassLoader cl = new ResourceFinderClassLoader(
+                new MapResourceFinder(classes),    // resourceFinder
+                ClassLoader.getSystemClassLoader() // parent
+        );
+
+        stopWatch.stop();
+        stopWatch.start("execute");
+
+        java.lang.reflect.Method[] methods = cl.loadClass("pkg2.RuleId").getMethods();
+        for (Method method : methods) {
+
+            if(method.getName().equals("meth")) {
+                Map map = new HashMap();
+                map.put("홍길동", 1);
+                System.out.println(method.invoke("meth", map));
+            }
+        }
+
+
+        stopWatch.stop();
+
+        System.out.println(stopWatch.prettyPrint());
 
         //Assert.assertEquals(77, cl.loadClass("pkg1.A").getDeclaredMethod("meth").invoke(null));
     }
--- src/test/java/janino/JaninoTest.java
+++ src/test/java/janino/JaninoTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 public class JaninoTest {
     @Test
     public void test() throws Exception {
+        long start = System.currentTimeMillis();
+
         ExpressionEvaluator ee = new ExpressionEvaluator("홍길동 > 1",
                 boolean.class,
                 new String[] {"홍길동"},
@@ -14,5 +16,7 @@ public class JaninoTest {
         Boolean value = (Boolean) ee.evaluate(new Object[] {new Integer(0)});
 
         System.out.println(value);
+
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
